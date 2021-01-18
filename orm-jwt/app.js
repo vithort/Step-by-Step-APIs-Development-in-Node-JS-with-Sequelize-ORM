03ae98a2cb6feecc189923b3966bf4349d17448e
Vithor <vithort@gmail.com>
2021-01-17 21:43:25 -0300

    Register user API
    Register user API

--- orm-jwt/app.js
+++ orm-jwt/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Sequelize = require('sequelize');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const JWT = require('jsonwebtoken');
 
 const app = express();
@@ -57,6 +58,47 @@ var User = sequelize.define(
 );
 
 User.sync(); // sync this model to database
+
+// register user api
+app.post('/user', (req, res) => {
+  let name = req.body.name;
+  let email = req.body.email;
+  let password = bcrypt.hashSync(req.body.password, 10); //hash value
+  let status = req.body.status;
+
+  User.findOne({ where: { email: email } })
+    .then((user) => {
+      // user should be empty if we have user with the email
+      if (user) {
+        res.status(200).json({
+          status: 0,
+          message: 'User already exists!',
+        });
+      } else {
+        User.create({
+          name: name,
+          email: email,
+          password: password,
+          status: status,
+        })
+          .then((response) => {
+            res.status(200).json({
+              status: 1,
+              message: 'User has been registered successfully!',
+            });
+          })
+          .catch((error) => {
+            res.status(500).json({
+              status: 0,
+              data: error,
+            });
+          });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 // defines default route of welcome page
 app.get('/', (req, res) => {
