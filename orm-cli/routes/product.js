32c25031d6b6afe8c38929612396b91b69010d17
Vithor <vithort@gmail.com>
2021-01-20 21:13:09 -0300

    Sequelize CLI - Models "findAll()" Object Properties
    Sequelize CLI - Models "findAll()"
    Object Properties

--- orm-cli/routes/product.js
+++ orm-cli/routes/product.js
@@ -4,22 +4,31 @@ const productModel = require('../models').Product;
 const router = express.Router();
 
 router.get('/products', (req, res) => {
-  productModel.findAll().then((data) => {
-    if (data) {
-      // we have data
-      res.status(200).json({
-        status: 1,
-        message: 'Products found',
-        data: data,
-      });
-    } else {
-      // we don't have data
-      res.status(200).json({
-        status: 0,
-        message: 'no Products found',
-      });
-    }
-  });
+  productModel
+    .findAll({
+      attributes: ['id', 'name'],
+      limit: 10, // total count of products we want at request
+      offset: 4, // we are setting our first index value
+      // order: [['id', 'DESC']],
+      order: [['name', 'ASC']],
+      where: {},
+    })
+    .then((data) => {
+      if (data) {
+        // we have data
+        res.status(200).json({
+          status: 1,
+          message: 'Products found',
+          data: data,
+        });
+      } else {
+        // we don't have data
+        res.status(200).json({
+          status: 0,
+          message: 'no Products found',
+        });
+      }
+    });
 });
 
 module.exports = router;
