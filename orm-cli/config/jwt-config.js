95cf24fb48d7693965b68ba243d63e6a19907230
Vithor <vithort@gmail.com>
2021-01-21 22:22:21 -0300

    Sequelize CLI - Student Login API
    Sequelize CLI - Student Login API

---
+++ orm-cli/config/jwt-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secret: 'onlinewebtutorkey',
+  expiresIn: 10000, // in seconds
+  notBefore: 30, // in seconds
+};
--- orm-cli/routes/student.js
+++ orm-cli/routes/student.js
@@ -1,11 +1,60 @@
 const express = require('express');
 const studentModel = require('../models').Student;
 const bcrypt = require('bcrypt');
+const JWT = require('jsonwebtoken');
+const jwtConfig = require('../config/jwt-config');
 const Sequelize = require('sequelize');
 
 const Op = Sequelize.Op;
 
 const router = express.Router();
+
+// login api for student
+router.post('/login', (req, res) => {
+  studentModel
+    .findOne({
+      where: {
+        email: {
+          [Op.eq]: req.body.email,
+        },
+      },
+    })
+    .then((student) => {
+      if (student) {
+        // login ok
+        let password = req.body.password;
+        if (bcrypt.compareSync(password, student.password)) {
+          // generate token method
+          let token = JWT.sign(
+            {
+              id: student.id,
+            },
+            jwtConfig.secret,
+            {
+              expiresIn: jwtConfig.expiresIn,
+              notBefore: jwtConfig.notBefore,
+            }
+          );
+          res.status(200).json({
+            status: 1,
+            message: 'Logged in successfull!',
+            token: token,
+          });
+        } else {
+          res.status(500).json({
+            status: 0,
+            message: 'Password didn`t match!',
+          });
+        }
+      } else {
+        // no student with this email address
+        res.status(500).json({
+          status: 0,
+          message: 'Student doesn`t exists with this email!',
+        });
+      }
+    });
+});
 
 // create student api
 router.post('/student', (req, res) => {
